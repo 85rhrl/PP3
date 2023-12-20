c2337e704eed6a2e45372ee55d2237669d80df66
Roberto Rodriguez <85rhrl@gmail.com>
2023-12-20 07:52:42 +0000

    add Game class and its init function

--- run.py
+++ run.py
@@ -11,4 +11,20 @@ def logo():
         \                                                             /
          \_______________________________________________WWS_________/""")
 
-logo()
+#logo()
+
+class Game:
+    """
+    Sets the game
+    """
+
+    def __init__(self):
+        self.board_size = 5
+        self.num_ships = 5
+        self.player_board = [["-" for x in range(board_size)] for y in range(board_size)]
+        self.cpu_board = [["-" for x in range(board_size)] for y in range(board_size)]
+        self.guesses = []
+        self.ships = []
+    
+    
+        
